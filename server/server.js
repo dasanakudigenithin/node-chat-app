4f0b2cc5cdc4b0b308819dea8c64da249b380d28
nithindj192 <nithindj192@gmail.com>
2018-04-05 14:05:14 +0530

    'jfbv'

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const path = require('path');
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 
-var app = express();
 const port = process.env.PORT || 3000;
 var public_path = path.join( __dirname , '../public');
+var app = express();
+var server = http.createServer(app);
+var io = socketio(server);
+
+io.on('connection',(socket)=>{
+console.log('new user connected.');
+socket.on('disconnect',()=>{
+    console.log('disconnected from serve');
+});
+})
+
+
+
 app.use(express.static(public_path));
 
-app.listen(port,()=>{
+server.listen(port,()=>{
 console.log("Listening on port 3000");
 });
