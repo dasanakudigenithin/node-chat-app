50263a8466c4d3dfb1ccc95074858cd38fd04cd4
nithindj192 <nithindj192@gmail.com>
2018-04-06 10:42:53 +0530

    'moment'

---
+++ playground/time.js
@@ -0,0 +1,5 @@
+var moment = require('moment');
+
+var date=moment();
+var showDate = date.format('hh:mm a');
+console.log(showDate);
--- public/js/index.js
+++ public/js/index.js
@@ -12,17 +12,18 @@ console.log('new Email.',email);
 });
 
 socket.on('newMessage',function(message){
-console.log('New Message: ',message);
+var formatedTime = moment(message.createdAt).format('hh:mm a');
 var li = jQuery('<li></li>');
-li.text(`${message.from} : ${message.text}`);
+li.text(`${message.from} ${formatedTime} : ${message.text}`);
 jQuery('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message){
+    var formatedTime = moment(message.createdAt).format('hh:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My loctaion</a>');
 
-    li.text(`${message.from} : `);
+    li.text(`${message.from} ${formatedTime} : `);
     a.attr('href',message.url);
     li.append(a);
     jQuery('#messages').append(li);
