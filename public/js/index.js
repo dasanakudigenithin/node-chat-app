9f3dde3a6002e0d13a322c5fed1344075e57ba73
nithindj192 <nithindj192@gmail.com>
2018-04-06 11:32:33 +0530

    'scrolling'

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,19 @@
 var socket = io();
+
+function scrolling(){
+var messages = jQuery('#messages');
+var newMessage = messages.children('li:last-child');
+var clientHeight = messages.prop('clientHeight');
+var scrollTop = messages.prop('scrollTop');
+var scrollHeight = messages.prop('scrollHeight');
+var newMessageHeight = newMessage.innerHeight();
+var lastMessageHeight = newMessage.prev().innerHeight();
+
+if( clientHeight + scrollTop + newMessageHeight + lastMessageHeight > scrollHeight){
+    messages.scrollTop(scrollHeight);
+}
+}
+
 socket.on('connect',function(){
     console.log('connected to server.');
 });
@@ -22,6 +37,7 @@ createdAt: formatedTime
 });
 
 jQuery('#messages').append(html);
+scrolling();
 
 });
 
@@ -35,6 +51,7 @@ socket.on('newLocationMessage', function(message){
     });
     
     jQuery('#messages').append(html);    
+    scrolling();
 
 });
 
